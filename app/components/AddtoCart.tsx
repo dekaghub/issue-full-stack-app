a3740b5aa0046fd09efd0a4c7b3abc9b95c5e68b
dekaghub <sdeka@bgsu.edu>
2024-03-17 02:40:55 +0000

    Fetched in server component then served client compo

---
+++ app/components/AddtoCart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddtoCart = () => {
+  return (
+    <div>
+        <button onClick={() => console.log("Clicked")}> Add me </button> 
+    </div>
+  )
+}
+
+export default AddtoCart
