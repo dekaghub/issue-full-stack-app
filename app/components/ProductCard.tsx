410fa1042d07fc93cf23dfac38013fff391cc48e
dekaghub <sdeka@bgsu.edu>
2024-03-17 03:27:18 +0000

    tailwing use

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import AddtoCart from './AddtoCart'
-import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-0 my-5 bg-sky-400 text-white text-xl hover:bg-sky-800'>
         <AddtoCart />
     </div>
   )
