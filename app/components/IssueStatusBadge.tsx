655e1ef6028df99112b4172e97c7b85f631ab81b
dekaghub <sdeka@bgsu.edu>
2024-03-19 23:22:01 +0000

    status badges work

--- app/components/IssueStatusBadge.tsx
+++ app/components/IssueStatusBadge.tsx
@@ -2,8 +2,9 @@ import { Status } from '@prisma/client'
 import { Badge } from '@radix-ui/themes'
 import React from 'react'
 
-const statusMap: Record<Status, {label: string, 
-                                 color: 'red'|'violet'|'green'}> = {
+const statusMap: Record<Status, 
+              {label: string, color: 'red'|'violet'|'green'}
+> = {
     OPEN: {label: 'Open', color: 'red'},
     IN_PROGRESS: {label: 'In Progress', color: 'violet'},
     CLOSED: {label: 'Closed', color: 'green'},
