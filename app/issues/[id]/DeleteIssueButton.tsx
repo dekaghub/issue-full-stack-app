7560243e67a7179befb257207ff5b6a3338a2444
dekaghub <sdeka@bgsu.edu>
2024-03-21 18:36:16 +0000

    Added Delete Button

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,8 +1,25 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
     return (
-        <Button color='red'>Delete Issue</Button>
+        <AlertDialog.Root>
+            <AlertDialog.Trigger>
+                <Button color='red'>Delete Issue</Button>
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+                <AlertDialog.Title>Confirm Delete?</AlertDialog.Title>
+                <AlertDialog.Description>Are you sure you want to delete this issue?</AlertDialog.Description>
+                <Flex direction="row" mt="4" gap="4">
+                    <AlertDialog.Action>
+                        <Button color="red">Delete Issue</Button>
+                    </AlertDialog.Action>
+                    <AlertDialog.Cancel>
+                        <Button color="iris">Cancel</Button>
+                    </AlertDialog.Cancel>
+                </Flex>
+            </AlertDialog.Content>
+        </AlertDialog.Root>
     )
 }
 
