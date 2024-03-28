7b1d274d038d85f01fae17c63ddf83ab458c15b9
dekaghub <sdeka@bgsu.edu>
2024-03-28 01:49:38 +0000

    Navbar layout changes signin signout buttons

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -4,34 +4,47 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classnames from 'classnames'
+import { useSession } from 'next-auth/react'
+import { Box, Container, Flex } from '@radix-ui/themes'
 
 const NavBar = () => {
 
-const currentPath = usePathname();
+    const currentPath = usePathname();
+    const { status, data: session } = useSession();
 
-const links = [
-    {label: 'Dashboard', href: "/"},
-    {label: 'Issues', href: "/issues"},
-]
+    const links = [
+        { label: 'Dashboard', href: "/" },
+        { label: 'Issues', href: "/issues" },
+    ]
 
-  return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-10 items-center'>
-        <Link href="/"><AiFillBug /></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-            <Link 
-                key={link.href}
-                className={classnames({
-                    'text-emerald-950 font-semibold': link.href === currentPath,
-                    'text-emerald-800': link.href !== currentPath,
-                    'hover:text-emerald-950 transition-colors': true
-                    })} 
-                href={link.href}>
-                    {link.label}
-            </Link>)}
-        </ul>
-    </nav>
-  )
+    return (
+        <nav className='border-b mb-5 px-5 py-3 items-center'>
+            <Container>
+                <Flex justify='between'>
+                    <Flex align="center" gap="4">
+                        <Link href="/"><AiFillBug /></Link>
+                        <ul className='flex space-x-6'>
+                            {links.map(link =>
+                                <li key={link.href}>
+                                    <Link
+                                        className={classnames({
+                                            'text-emerald-950 font-semibold': link.href === currentPath,
+                                            'text-emerald-800': link.href !== currentPath,
+                                            'hover:text-emerald-950 transition-colors': true
+                                        })}
+                                        href={link.href}>
+                                        {link.label}
+                                    </Link></li>)}
+                        </ul>
+                    </Flex>
+                    <Box>
+                        {status === "authenticated" && <Link href="/api/auth/signout">Sign Out</Link>}
+                        {status === "unauthenticated" && <Link href="/api/auth/signin">Sign In</Link>}
+                    </Box>
+                </Flex>
+            </Container>
+        </nav>
+    )
 }
 
 export default NavBar
