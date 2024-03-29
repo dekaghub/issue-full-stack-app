a8ed072b84f2ee6f6fba75a8e5b2367c578ddcd7
dekaghub <sdeka@bgsu.edu>
2024-03-29 22:57:19 +0000

    Dashboard build latest & summary

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,36 @@
+import { Status } from '@prisma/client'
+import { Card, Flex, Text } from '@radix-ui/themes'
+import Link from 'next/link'
+
+interface Props {
+    open: number,
+    inProgress: number,
+    closed: number
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+    const summaries: { label: string, value: number, status: Status }[] = [
+        { label: 'Open Issues', value: open, status: 'OPEN'},
+        { label: 'Closed Issues', value: closed, status: 'CLOSED'},
+        { label: 'In Progress Issues', value: inProgress, status: 'IN_PROGRESS'},
+    ]
+    return (
+        <Flex gap="4">
+            {summaries.map(summary => (
+                <Card key={summary.value}>
+                    <Flex direction="column">
+                        <Link 
+                            className='text-lg font-thin'
+                            href={`/issues/list?status=${summary.status}`}
+                            >
+                            {summary.label}
+                        </Link>
+                        <Text size="4" className='font-semibold'>{summary.value}</Text>
+                    </Flex>
+                </Card>
+            ))}
+        </Flex>
+    )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: 'OPEN' } })
+  const closed = await prisma.issue.count({ where: { status: 'CLOSED' } })
+  const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } })
   return (
-    <LatestIssues />
+    <>
+      <IssueSummary closed={closed} inProgress={inProgress} open={open} />
+      <LatestIssues />
+    </>
   )
 }
