092702ac44ab948b5b69577299920a5e4f401313
dekaghub <sdeka@bgsu.edu>
2024-03-29 21:15:35 +0000

    fixed UI things

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -4,18 +4,20 @@ import { Link, IssueStatusBadge } from "@/app/components";
 import NextLink from "next/link";
 import IssueActions from "./IssueActions";
 import { Issue, Status } from "@prisma/client";
-import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { ArrowDownIcon, ArrowUpIcon } from "@radix-ui/react-icons";
 
-const IssuesPage = async ({ searchParams }: { searchParams: { status: Status, orderBy: keyof Issue } }) => {
+const IssuesPage = async ({ searchParams }: { searchParams: { status: Status, orderBy: keyof Issue , sortOrder: "asc" | "desc"} }) => {
     const columns: { label: string, value: keyof Issue, className?: string }[] = [
         { label: 'Issue', value: 'title' },
         { label: 'Status', value: 'status', className: "hidden md:table-cell" },
         { label: 'Created', value: 'createdAt', className: "hidden md:table-cell" }
     ]
+    const orderBy = searchParams.orderBy? { [searchParams.orderBy]: searchParams.sortOrder }: undefined;
+    const toggleOrder = () => {
+        return !searchParams.sortOrder || searchParams.sortOrder === "desc" ? "asc" : "desc";
+    };
     const statuses = Object.values(Status)
     const status = statuses.includes(searchParams.status) ? searchParams.status : undefined
-    const orderBy = columns.map(column => column.value)
-                            .includes(searchParams.orderBy) ? {[searchParams.orderBy]: "asc"} : undefined
     const issues = await prisma.issue.findMany({
         where: {
             status
@@ -32,11 +34,11 @@ const IssuesPage = async ({ searchParams }: { searchParams: { status: Status, or
                         {columns.map(column => (
                             <Table.ColumnHeaderCell key={column.value} className={column.className}>
                                 <NextLink href={{
-                                    query: { ...searchParams, orderBy: column.value }
+                                    query: { ...searchParams, orderBy: column.value, sortOrder: toggleOrder() }
                                 }}>
                                     {column.label}
                                 </NextLink>
-                                { column.value === searchParams.orderBy && <ArrowUpIcon className="inline"/>}
+                                { column.value === searchParams.orderBy && (searchParams.sortOrder === "asc" ? <ArrowUpIcon className="inline" /> : <ArrowDownIcon className="inline" />)}
                             </Table.ColumnHeaderCell>
                         ))}
                     </Table.Row>
